b21b3eaf1f51e50bef64436a57420399fd89e69a
Jaqueline Camilo Dias <jaque20050@hotmail.com>
2022-01-05 18:03:01 -0300

    Configurando a rota no módulo de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
+import { LoginModule } from './autenticacao';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    LoginModule,
+    
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
